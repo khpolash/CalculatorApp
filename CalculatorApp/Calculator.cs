86f4a91de075ed5c161845d813c9f531423ce44c
khpolash <khasan994@gmail.com>
2015-05-13 16:41:46 +0600

    Initial CalculatorApp

---
+++ CalculatorApp/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    class Calculator
+    {
+        public double firstNumber;
+        public double secondNumber;
+        
+        public double Add(double fNumber, double sNumber)
+        {
+            return (fNumber + sNumber);
+        }
+        public double Sub(double fNumber, double sNumber)
+        {
+            return (fNumber - sNumber);
+        }
+        public double Mul(double fNumber, double sNumber)
+        {
+            return (fNumber * sNumber);
+        }
+        public double Div(double fNumber, double sNumber)
+        {
+            return (fNumber / sNumber);
+        }
+       
+    }
+}
---
+++ CalculatorApp/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Calculator aCalculator = new Calculator();
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if (firstNumberTextBox.Text!="" && secondNumberTextBox.Text!="")
+            {
+                aCalculator.firstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+                aCalculator.secondNumber = Convert.ToDouble(secondNumberTextBox.Text);
+
+                resulrtTextBox.Text = Convert.ToString(aCalculator.Add(aCalculator.firstNumber, aCalculator.secondNumber));
+            }
+            else
+            {
+                MessageBox.Show("Please enter First & Second Number");
+            }
+           
+        }
+
+        private void subButton_Click(object sender, EventArgs e)
+        {
+            aCalculator.firstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            aCalculator.secondNumber = Convert.ToDouble(secondNumberTextBox.Text);
+
+            resulrtTextBox.Text = Convert.ToString(aCalculator.Sub(aCalculator.firstNumber, aCalculator.secondNumber));
+        }
+
+        private void mulButton_Click(object sender, EventArgs e)
+        {
+            aCalculator.firstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            aCalculator.secondNumber = Convert.ToDouble(secondNumberTextBox.Text);
+
+            resulrtTextBox.Text = Convert.ToString(aCalculator.Mul(aCalculator.firstNumber, aCalculator.secondNumber));
+        }
+
+        private void divButton_Click(object sender, EventArgs e)
+        {
+            aCalculator.firstNumber = Convert.ToDouble(firstNumberTextBox.Text);
+            aCalculator.secondNumber = Convert.ToDouble(secondNumberTextBox.Text);
+
+            resulrtTextBox.Text = Convert.ToString(aCalculator.Div(aCalculator.firstNumber, aCalculator.secondNumber));
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            firstNumberTextBox.Clear();
+            secondNumberTextBox.Clear();
+            resulrtTextBox.Clear();
+        }
+    }
+}
